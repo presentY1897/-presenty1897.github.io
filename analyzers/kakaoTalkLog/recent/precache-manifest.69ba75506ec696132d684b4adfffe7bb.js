d22cdc3e4078a6426243a9d61c9f1996e2d7a753
presenty1897 <presenty1897@gmail.com>
2020-11-29 10:35:48 +0900

    add kakaoTalkData Analysis mockup

---
+++ analyzers/kakaoTalkLog/recent/precache-manifest.69ba75506ec696132d684b4adfffe7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7cbe7b1479684554153ac10956599c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd6b4ffee76f4df8633",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9cfac6c8ca7e85894a9f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bd6b4ffee76f4df8633",
+    "url": "/static/js/2.08e67da9.chunk.js"
+  },
+  {
+    "revision": "4fd528cd8e9ca98dc01eafc45195a1cf",
+    "url": "/static/js/2.08e67da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfac6c8ca7e85894a9f",
+    "url": "/static/js/main.0bdf8f8e.chunk.js"
+  },
+  {
+    "revision": "adffebbcbf2c15aff5f7",
+    "url": "/static/js/runtime-main.f062143d.js"
+  }
+]);
